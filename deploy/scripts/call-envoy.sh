23b7795b97c92a23f83a649b8313b98f97d47b16
Arthas <kravchinskij_sa@spbstu.ru>
2024-12-27 14:11:28 +0300

    deploy

--- deploy/scripts/call-envoy.sh
+++ deploy/scripts/call-envoy.sh
@@ -5,4 +5,4 @@ TOKEN=$(./scripts/keycloak-tokens.sh)
 curl -H "Authorization: Bearer ${TOKEN}" \
   -H "X-Request-ID: 1234" \
   -H "x-client-request-id: 1235" \
-  http://localhost:8080/v1/parts/
+  http://localhost:8080/v1/models/
